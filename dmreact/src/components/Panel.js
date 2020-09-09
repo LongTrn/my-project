475422aa2f05ccf6b98b2db97934b21c3615f48e
Long <longjax1401@gmail.com>
2020-09-09 22:02:15 +0700

    ADD: many attributes of list item

--- dmreact/src/components/Panel.js
+++ dmreact/src/components/Panel.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import TableInputs from './Table.input';
 
 function addRow(e) {
-        
+  return;
 }
 
 function Panel() {
   return (
     <div className="Panel">
         <form>
-            <input type="number" placeholder={'Add minimum SUPPORT value'}></input>
-            <input type="number" placeholder={'Add minimum CONFIDENCE value'}></input>
-            <input type="button" onClick={addRow}>Add Data</input>
-            <input type="submit"></input>
+          <input type="number" placeholder={'Add minimum SUPPORT value'}></input>
+          <input type="number" placeholder={'Add minimum CONFIDENCE value'}></input>
+          <input type="button" value="Add Data"></input>
+          <input type="submit"></input>
+          <TableInputs/>
         </form>
     </div>
   );
---
+++ dmreact/src/components/Table.input.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+
+import '../styles/Table.input.css';
+
+function TableInputs(props) {
+    const [rows, setRows] = useState(2)
+    const [cols, setCols] = useState(2)
+    const [state, setState] = useState({
+        listAttributes: [{attribute:'New Attribute'}]
+    })
+    let attr = []
+    for (let i = 0; i < cols||props.cols; i++){
+        attr.push(
+            <td>{rows}</td>
+        )
+    }
+
+    function addAttributes() { 
+        let newAttr = (state.listAttributes)
+        console.log(newAttr)
+        setState((prevState) => ({
+            ...prevState,
+            listAttributes: [
+                ...prevState.listAttributes,
+                {attribute:'New Attribute'}
+            ]
+        }))
+
+    }
+
+
+    const { listAttributes } = state;
+    return (
+        <div className="Table.input">
+            <table>
+                <thead>
+                    { 
+                        listAttributes.length > 0 && listAttributes.map((item) => {
+                            console.log(item)
+                            return <input type="text" placeholder={item.attribute}/>
+                    })}
+                    <input type="button" onClick={addAttributes} value='Attribute'/>
+                </thead>
+                {/* <tbody>
+                    <tr>
+                        {attr}
+                    </tr>
+                </tbody> */}
+            </table>
+        </div>
+    );
+}
+
+export default TableInputs;
